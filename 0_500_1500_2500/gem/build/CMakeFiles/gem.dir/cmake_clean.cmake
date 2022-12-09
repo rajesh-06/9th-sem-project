d7e4e5677113f88c8a143e2b0eac4786d45ddc7c
B Rajesh Achari <42206081+rajesh-06@users.noreply.github.com>
2022-12-09 15:42:06 +0530

    Add files via upload

---
+++ 0_500_1500_2500/gem/build/CMakeFiles/gem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gem.dir/gem.C.o"
+  "CMakeFiles/gem.dir/gem.C.o.d"
+  "gem"
+  "gem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
